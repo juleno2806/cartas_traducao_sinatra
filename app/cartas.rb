d9baaeb2342bd02514343dee50f467c52a945e9d
Paula <paulaperussi@gmail.com>
2020-03-06 22:15:31 -0300

    Aula de Ruby

--- app/cartas.rb
+++ app/cartas.rb
@@ -5,4 +5,9 @@ set :static, true
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 get '/' do
+  @cartas = []
+  @cartas << Carta.new(ingles: 'Red', portugues: 'Vermelho')
+  @cartas << Carta.new(ingles: 'Green', portugues: 'Verde')
+  erb :index
+
 end
